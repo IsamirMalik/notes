a97f96cdc57772d054e5922801825b729a29ff97
sameer malik <samirmalik591@gmail.com>
2025-09-19 12:24:22 +0530

    add lexicographical sorting of number in recursion

---
+++ DSA/recursion/lexicographical-nums.js
@@ -0,0 +1,30 @@
+
+/*
+
+Given an integer n , return all numbers in the range [1,n] sorted in lexicographical order ( generalistion of alphabetical order of the dictionaries) .
+must => TC -> O(n) and SC -> O(1)
+
+*/
+
+let arr;
+
+function lexicographicalOrder(i, n) {
+    if (i > n) return;
+    if (i != 0) {
+        arr.push(i)
+    }
+
+    for (let j = ((i == 0) ? 1 : 0); j <= 9; j++) {
+        lexicographicalOrder(i * 10 + j, n);
+    }
+}
+
+function callingFunction(n) {
+    arr = [];
+    lexicographicalOrder(0, n);
+    return arr;
+}
+
+let functionCall = callingFunction(30);
+console.log(functionCall)
+
