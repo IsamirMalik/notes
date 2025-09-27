ad072a0bdc2776c4e112ff5081483b86d398a61c
sameer malik <samirmalik591@gmail.com>
2025-09-27 16:11:11 +0530

    add type conversion

---
+++ javaScript/06-type-conversion.js
@@ -0,0 +1,39 @@
+// Type Conversion
+
+let amount = 100;
+
+
+
+// ~~~~~~ Convert String to Number ~~~~~~~ //
+
+// amount = parseInt(amount);
+// amount = parseFloat(amount);  //-> if the number is a decimal number
+// amount = +(amount);
+// amount = Number(amount);
+
+
+// ~~~~~~ Convert Number to String ~~~~~~~ //
+
+// amount = amount.toString();
+// amount = String(amount);
+
+
+// ~~~~~~ Convert Number to Boolean ~~~~~~~ //
+
+// amount = Boolean(amount);  // zero a falsy value
+
+
+
+// NaN -> property of global window object , 5 operations gives us the NaN .
+
+// amount = 'hello';
+// amount = Boolean(amount);
+// console.log(Math.sqrt(-1));
+// console.log(1 + NaN);
+// console.log(undefined + undefined);
+// console.log('foo' / 3);
+
+
+
+
+console.log(amount ,typeof amount);
