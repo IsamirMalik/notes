a76075141bd46c1a877be2bd14059a7369179de5
sameer malik <samirmalik591@gmail.com>
2025-09-27 16:11:30 +0530

    add operatores in Js

---
+++ javaScript/07-operators.js
@@ -0,0 +1,49 @@
+// Arithmetic Operators
+// + , - , * , / , %
+
+// Concatenation
+
+let str1 = 'Hello'
+let str2 = 'World'
+
+// let final = str1 + ' ' + str2 ;
+
+// Exponent
+let x = 2 ** 3 ;
+
+// Increment & Decrement
+let a = 10
+a++ // 11
+++a // 12
+
+a-- // 11
+--a // 10
+
+// Assignment Operators
+// = , += , -= , *= , /= , %= , **=
+
+let p = 10;
+p += 10;
+p -= 10;
+p *= 10;
+p /= 10;
+p %= 10;
+p **= 10;
+
+console.log(p);
+
+// Comparison Operators
+// == , != , > , < , >= , <= , === , !==  
+
+let y = 2 == '2';
+y = 2 === '2';
+
+y = 2 != '2';
+y = 2 !== '2';
+
+y = 2 > '2';
+y = 2 < '2';
+
+y = 2 >= '2';
+y = 2 <= '2';
+console.log(y);
