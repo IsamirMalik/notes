c2b3dcb197aa66cea45d5fc0336d67ffc5d543cd
sameer malik <samirmalik591@gmail.com>
2025-09-28 17:42:11 +0530

    add number challenge

---
+++ javaScript/challenges/02-number-challenge.js
@@ -0,0 +1,22 @@
+let x = Math.floor(Math.random() * 100 + 1);
+let y = Math.floor(Math.random() * 50 + 1);
+
+// Get the sum
+let sumOutput = x + y;
+console.log(`${x} + ${y} = ${sumOutput}`);
+
+// Get the difference
+let differenceOutput = x - y;
+console.log(`${x} - ${y} = ${differenceOutput}`);
+
+// Get the product
+let productOutput = x * y;
+console.log(`${x} * ${y} = ${productOutput}`);
+
+// Get the quotient
+let quotientOutput = x / y;
+console.log(`${x} / ${y} = ${quotientOutput}`);
+
+// Get the remaider
+let remaiderOutput = x % y;
+console.log(`${x} % ${y} = ${remaiderOutput}`);
