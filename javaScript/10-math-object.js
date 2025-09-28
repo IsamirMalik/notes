f8879029aa94272a6984314945c82f6f3d1b25d7
sameer malik <samirmalik591@gmail.com>
2025-09-28 17:40:41 +0530

    add math object

---
+++ javaScript/10-math-object.js
@@ -0,0 +1,33 @@
+let x ;
+
+x = Math.sqrt(16); // 4
+console.log(x);
+
+x = Math.pow(2, 3); // 8
+console.log(x);
+
+x = Math.abs(-10); // 10
+console.log(x);
+
+x = Math.round(3.5); // 4
+console.log(x);
+
+x = Math.ceil(3.5); // 4
+console.log(x);
+
+x = Math.floor(3.5); // 3
+console.log(x);
+
+x = Math.min(10, 20, 30, 40); // 10
+console.log(x);
+
+x = Math.max(10, 20, 30, 40); // 40
+console.log(x);
+
+x = Math.random();
+console.log(x); // random number between 0 and 1 ;
+
+x = Math.floor(Math.random() * 100 + 1); // random number between 1 and 100
+console.log(x);
+
+
