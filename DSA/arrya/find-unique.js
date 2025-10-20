c701c21ee5a02f393bb9d2c53a8fb5806dbb2af9
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-10-21 00:02:03 +0530

    added solution for find unique in the array

---
+++ DSA/arrya/find-unique.js
@@ -0,0 +1,25 @@
+// Given an array of integers , we need to find the unique elements in the array , every other element is repeated .
+
+const findUnique = (array) => {
+    let unique;
+    // array = array.sort((a,b) => a - b);
+    // console.log(array)
+    // for(let index = 0 ; index < array.length ; index += 2){
+    //     if(array[index] !== array[index + 1]){
+    //         unique = array[index];
+    //         break;
+    //     }
+    // }
+
+    // using XOR 
+
+    for(let int of array){
+        unique ^= int
+    }
+
+    return unique;
+}
+
+let array = [1,2,3,5,4,5,3,2,1];
+let final = findUnique(array);
+console.log(final)
